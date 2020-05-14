1ec2b8a582d6ce6eabe401f1f547dd32c113cfe2
zubquzaini <zubliquzaini@gmail.com>
2020-05-14 10:04:38 +0800

    prescribed by zubquzaini

---
+++ icuba.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "salam dari selangor" << endl;
+    
+    cout << "Tumpang dominasi jap yaa" << endl;
+    for(int x=0; x<1000; x++) {
+        cout << "I love u" << endl;
+    }
+    return 0;
+}
+
